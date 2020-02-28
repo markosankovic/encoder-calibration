bd5df3ba9416827356e7f20e6b392d71e3c19f61
Marko Sanković <msankovic@synapticon.com>
2020-02-28 09:25:27 +0100

    Variable number of ports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BehaviorSubject } from 'rxjs';
 import { MotionMasterMessage } from '@synapticon/motion-master-client';
+import ordinal from 'ordinal';
 
 import './App.css';
 import Footer from './Footer';
@@ -18,12 +19,24 @@ class App extends React.Component {
     this.state = {
       alive: false,
       motionMasterState: '',
+      ports: 2,
     };
+
+    this.incPorts = this.incPorts.bind(this);
+    this.decPorts = this.decPorts.bind(this);
   }
 
   componentDidMount() {
     alive$.subscribe(alive => this.setState({ alive }));
     systemEvent$.subscribe(systemEvent => this.setState({ motionMasterState: MotionMasterMessage.Status.SystemEvent.State[systemEvent.state] }));
+  }
+
+  incPorts() {
+    this.setState({ ports: this.state.ports === 9 ? 9 : this.state.ports + 1 });
+  }
+
+  decPorts() {
+    this.setState({ ports: this.state.ports === 1 ? 1 : this.state.ports - 1 });
   }
 
   render() {
@@ -37,41 +50,31 @@ class App extends React.Component {
       </div>
     ) : null;
 
-    // const magneticEncoderAlignment = this.state.alive ? (
-    //   <div className="row">
-    //     <div className="col">
-    //       <MagneticEncoderAlignment encoderPort="0"></MagneticEncoderAlignment>
-    //     </div>
-    //   </div>
-    // ) : null;
+    let actions;
+    let encoders;
 
-    // const narrowAngleCalibration = this.state.alive ? (
-    //   <div className="row">
-    //     <div className="col">
-    //       <NarrowAngleCalibration encoderPort="1"></NarrowAngleCalibration>
-    //     </div>
-    //   </div>
-    // ) : null;
+    if (this.state.alive) {
 
-    const encoders = this.state.alive ? (
-      <div>
-        <div className="row">
-          <div className="col">
-            <h5>Encoder port 0</h5>
-            <MagneticEncoderAlignment encoderPort="0"></MagneticEncoderAlignment>
-            <NarrowAngleCalibration encoderPort="0"></NarrowAngleCalibration>
+      actions = (
+        <div className="row mb-3">
+          <div className="col d-flex align-items-center">
+            <div>NUMBER OF PORTS:</div>
+            <button type="button" className="btn btn-outline-success ml-3" onClick={this.incPorts}>INC</button>
+            <button type="button" className="btn btn-outline-danger ml-1" onClick={this.decPorts}>DEC</button>
           </div>
         </div>
-        <hr />
-        <div className="row">
+      );
+
+      encoders = Array(this.state.ports).fill().map((_, i) => (
+        <div className="row encoder mb-4" key={i}>
           <div className="col">
-            <h5>Encoder port 1</h5>
-            <MagneticEncoderAlignment encoderPort="1"></MagneticEncoderAlignment>
-            <NarrowAngleCalibration encoderPort="1"></NarrowAngleCalibration>
+            <h5>{ordinal(i + 1)} ENCODER PORT</h5>
+            <MagneticEncoderAlignment encoderPort={i}></MagneticEncoderAlignment>
+            <NarrowAngleCalibration encoderPort={i}></NarrowAngleCalibration>
           </div>
         </div>
-      </div>
-    ) : null;
+      ))
+    }
 
     return (
       <div className="container-fluid">
@@ -82,7 +85,10 @@ class App extends React.Component {
           </div>
         </div>
 
+
         <hr />
+
+        {actions}
 
         {encoders}
 
